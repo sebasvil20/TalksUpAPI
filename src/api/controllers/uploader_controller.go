6c3f299e59f84333bb1269de328d0e203093debd
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-05-04 15:21:33 -0500

    Add file extensions validator and link fixes

--- src/api/controllers/uploader_controller.go
+++ src/api/controllers/uploader_controller.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -21,9 +20,7 @@ type UploaderController struct {
 func (ctrl *UploaderController) UploadFile(c *gin.Context) {
 	var form models.Form
 	_ = c.ShouldBind(&form)
-	openedFile, _ := form.File.Open()
-	file, _ := ioutil.ReadAll(openedFile)
-	url, err := ctrl.UploaderService.UploadImage(file)
+	url, err := ctrl.UploaderService.UploadImage(form)
 	if err != nil {
 		utils.HandleResponse(c, http.StatusBadRequest, err.Error())
 		return
--- src/api/services/uploader_service.go
+++ src/api/services/uploader_service.go
@@ -4,33 +4,43 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/google/uuid"
 	"github.com/sebasvil20/TalksUpAPI/src/api/config"
+	"github.com/sebasvil20/TalksUpAPI/src/api/models"
+	"github.com/sebasvil20/TalksUpAPI/src/api/utils"
 )
 
 type IUploaderService interface {
-	UploadImage(file []byte) (string, error)
+	UploadImage(form models.Form) (string, error)
 }
 
 type UploaderService struct {
 }
 
-func (srv *UploaderService) UploadImage(file []byte) (string, error) {
+func (srv *UploaderService) UploadImage(form models.Form) (string, error) {
+	formFile, fileExt, fileExtNoPoint := utils.GetFileInfo(form)
+	if !utils.IsImage(fileExt) {
+		return "", fmt.Errorf("file extension isn't valid, try with .png, .jpg, .jpeg")
+	}
+
+	openedFile, _ := formFile.Open()
+	file, _ := ioutil.ReadAll(openedFile)
 	fileName, _ := uuid.NewUUID()
 	object := s3.PutObjectInput{
 		Bucket:      aws.String("talksupcdn"),
-		Key:         aws.String(fmt.Sprintf("%v.png", fileName.String())),
+		Key:         aws.String(fmt.Sprintf("%v.%v", fileName.String(), fileExtNoPoint)),
 		Body:        bytes.NewReader(file),
 		ACL:         "public-read",
-		ContentType: aws.String("image/png"),
+		ContentType: aws.String(fmt.Sprintf("image/%v", fileExtNoPoint)),
 	}
 	_, err := config.S3Client.PutObject(context.TODO(), &object)
 	if err != nil {
-		fmt.Println(err.Error())
+		return "", fmt.Errorf("couldn't upload file, try again later or contact admins")
 	}
 
-	return fmt.Sprintf("%v%v.png", config.CDNBaseURL, fileName), nil
+	return fmt.Sprintf("%v/%v.%v", config.CDNBaseURL, fileName, fileExtNoPoint), nil
 }
